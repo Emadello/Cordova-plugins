cef127b720113657d735fefdaeb2e5245913acad
Emadello <emad_fankoosh@hotmail.com>
2014-08-20 20:57:39 +0300

    Create ModalActivity.java

---
+++ src/kr/co/purpleworks/cordova/modal/ModalActivity.java
@@ -0,0 +1,27 @@
+package kr.co.purpleworks.cordova.modal;
+
+import org.apache.cordova.CordovaActivity;
+
+import android.os.Bundle;
+
+import com.Example.Package.R; // Chage this line to your original package src
+
+public class ModalActivity extends CordovaActivity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        this.overridePendingTransition(R.anim.bottom_in, R.anim.hold);
+        super.init();
+
+        String url = getIntent().getStringExtra(Modal.PARAM_LOAD_URL);
+        super.loadUrl(url);
+    }
+
+    @Override
+    public void finish() {
+    	super.finish();
+    	this.overridePendingTransition(R.anim.hold, R.anim.bottom_out);
+    }
+}
